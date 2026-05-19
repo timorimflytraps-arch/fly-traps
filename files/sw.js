b55e0753045015ad55b95ca251d3ca96c6a902b9
timorimflytraps-arch <timorimflytraps@gmail.com>
2026-05-19 12:00:37 +0300

    Delete files directory

--- files/sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'timorim-flies-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
